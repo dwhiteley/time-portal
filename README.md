375dacea0de76c88e8fb4f09e795776bb0295d6f
Daniel Whiteley <daniel.whiteley@gmail.com>
2016-07-19 21:37:11 -0700

    Improved BMP-to-raw conversion

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ This project uses the following Arduino libraries:
 * [Adafruit_ILI9341](https://github.com/dwhiteley/Adafruit_ILI9341) (forked)
 * [Adafruit-GFX-Library](https://github.com/adafruit/Adafruit-GFX-Library)
 
+The sketch uses 320x240 images in a raw 16-bit color format. The script utils/bmpto565raw.py will convert 24-bit color bitmaps into the needed format. The script expects the following conditons:
+* The bitmaps are already pre-sized to 320x240
+* The bitsmaps are enumerated XXXXXXa.bmp, where XXXXXX is a number
+* The bitmaps are located in the img/originals folder in the project
+
+The script will create XXXXXXb.raw files in the img/raw folder. These files should be loaded into the SD-Card that you insert into the display.
--- utils/bmpto565raw.py
+++ utils/bmpto565raw.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import sys, struct
+from glob import glob
 
 VERIFY = False
 
@@ -10,21 +11,20 @@ def convert(in_name, out_name):
     gs = im.getdata(1)
     bs = im.getdata(2)
     
-    rgb565 = [(r/8)*2048 + (g/4)*32 + (b/8) for (r,g,b) in zip(rs, gs, bs)]
+    rgb565 = [(r>>3)*2048 + (g>>2)*32 + (b>>3) for (r,g,b) in zip(rs, gs, bs)]
     
-    
-    
-    f = file(out_name, "wb")
+    f = open(out_name, "wb")
     #Big Endian
     # f.write(struct.pack(">%dH" % len(rgb565), *rgb565))
     #Little Endian
     f.write(struct.pack("<%dH" % len(rgb565), *rgb565))
     f.close()
 
-for i in range(3):
-    in_name = "img/originals/%06da.bmp"%i
-    out_name = "img/raw/%06db.raw"%i
-    convert(in_name, out_name)
+for i in glob('../img/originals/*.bmp'):
+    #in_name = "img/originals/%06da.bmp"%i
+    #out_name = "img/raw/%06db.raw"%i
+    o = i.replace('originals','raw').replace('a.bmp','b.raw')
+    convert(i, o)
 
 # verify
 if VERIFY:
